1b19afc2b497dbd2b291ef670107eb7fadf99b63
root <root@srv1375773.hstgr.cloud>
2026-08-21 07:38:42 +0000

    Redirect link

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+
+  async redirects (){
+      return[
+        {
+          source: '/services/chronic-disease-management',
+          destination: '/services/chronic-disease',
+          permanent: true,
+        },
+        {
+          source: '/bookconsultationcd',
+          destination: '/bookconsultation',
+          permanent: true,
+        }
+      ]
+  }
 };
 
 export default nextConfig;
